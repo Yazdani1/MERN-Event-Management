238cd8626c8b6f68559b24829b16899caa64c9c6
Yazdani <yaz4dani@gmail.com>
2022-01-16 21:15:57 +0200

    search feature

--- client/src/components/HomePage/Home.js
+++ client/src/components/HomePage/Home.js
@@ -1,14 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import FirstSection from "./FirstSection";
 import AllEvents from "./AllEvents";
 import UserList from "./UserList";
 import Totalpostcount from "./TotalPostCount";
+import { getallEvents, getallEventorganizers } from "./APIAllevents";
 
 const Home = () => {
+  const [allevent, setAllevents] = useState([]);
+  const [allusers, setAllusers] = useState([]);
+
+  const loadallEvents = () => {
+    getallEvents()
+      .then((result) => {
+        setAllevents(result);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const loadallUsers = () => {
+    getallEventorganizers()
+      .then((result) => {
+        setAllusers(result);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  useEffect(() => {
+    loadallEvents();
+    loadallUsers();
+  }, []);
+
   return (
     <div>
       <FirstSection />
-      <Totalpostcount/>
+      <Totalpostcount totalpost={allevent.length} totaluser={allusers.length} />
       <AllEvents />
       <UserList />
     </div>
